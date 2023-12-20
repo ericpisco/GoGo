65a1e93f84bcbbfdcf063c25d5e41537a0383a2d
Hakizimana Eric Pisco <hakizericpisco@gmail.com>
2023-12-20 10:55:58 +0200

    error handling

--- register.js
+++ register.js
@@ -2,12 +2,16 @@ const express = require('express');
 const path = require('path');
 const mysql = require('mysql');
 const bcrypt = require('bcrypt');
+const ejs = require('ejs');
 
 const app = express();
 const port = 5500;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 const db = mysql.createConnection({
     host: 'localhost',
@@ -27,34 +31,37 @@ db.connect(err => {
 // Serve static files from the root directory
 app.use(express.static(__dirname));
 
+// Helper function to render the registration page using EJS
+function renderRegistrationPage(res, { error, name, phone }) {
+    res.render('register', { error, name, phone });
+}
+
 // API endpoint
 app.post('/register', async (req, res) => {
     const { name, phone, password } = req.body;
 
     try {
-        // Hash the password
         const hashedPassword = await bcrypt.hash(password, 10);
 
         const checkQuery = 'SELECT * FROM users WHERE phone = ?';
         db.query(checkQuery, [phone], async (checkErr, checkResult) => {
             if (checkErr) {
                 console.error('Error checking phone number:', checkErr);
-                res.status(500).json({ message: 'Registration failed' });
+                renderRegistrationPage(res, { error: 'Registration failed' });
             } else if (checkResult.length > 0) {
-                // Remain on the register.html page with an error message
-                res.redirect('/register.html?error=Phone%20number%20already%20registered');
+                const errorMessage = 'Phone number already registered';
+                renderRegistrationPage(res, { error: errorMessage, name, phone });
             } else {
                 const insertQuery = 'INSERT INTO users (name, phone, password) VALUES (?, ?, ?)';
                 await db.query(insertQuery, [name, phone, hashedPassword]);
                 console.log('User registered');
 
-                // Redirect to the login.html page with a success message
                 res.redirect('/login.html?message=User%20successfully%20registered');
             }
         });
     } catch (error) {
         console.error('Error hashing password:', error);
-        res.status(500).json({ message: 'Registration failed' });
+        renderRegistrationPage(res, { error: 'Registration failed' });
     }
 });
 
